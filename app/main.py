ab30f3747f28f22ff3485628f8a0e4b195bf025b
L0nkk <129335806+L0nkk@users.noreply.github.com>
2023-12-01 14:15:21 -0300

    api con metodos pedidos/usuarios
    app/main.py

--- app/main.py
+++ app/main.py
@@ -89,3 +89,80 @@ async def get_image(image_id: str):
     if (image := await mongo['images'].find_one({'image_id': image_id})) is None:
         raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND)
     return StreamingResponse(BytesIO(image['data']), media_type=image['content_type'])
+
+@app.post("/pedido/")
+async def create_item(pedido: Pedido):
+    try:
+        pedido_data = pedido.dict()
+        pedido_data['pedido_id'] = str(uuid4())
+        pedido_data['estado'] = 1
+        #item_data["id"] = id
+        await mongo['pedidos'].insert_one(pedido_data)
+        pedido_data.pop('_id')
+        return pedido_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    
+@app.get("/pedidos/")
+async def read_all_items(limit: int = 100):
+    pedidos = await mongo['pedidos'].find().to_list(limit)
+    try:
+        for item in pedidos:
+             item.pop('_id', None)
+        return pedidos
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
+@app.get("/pedido/{pedido_id}")
+async def read_item(pedido_id: str):
+    pedidos = await mongo['pedidos'].find_one({'pedido_id': pedido_id})
+    if pedidos is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    pedidos.pop('_id')
+    return pedidos
+
+# CRUD Usuario :
+# Create
+@app.post('/usuario')
+async def create_user(user: User):
+    user_id = str(uuid4())
+    user_data = user.dict()
+    user_data['user_id'] = user_id
+    await mongo['usuarios'].insert_one(user_data)
+    user_data.pop('_id')
+    return user_data
+# Read
+@app.get('/usuario/{user_id}')
+async def get_user(user_id: str):
+    user = await mongo['usuarios'].find_one({'user_id': user_id})
+    if user is None:
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND)
+    user.pop('_id')
+    return user
+# Read All
+@app.get('/usuarios')
+async def get_all_users(limit: int = 100):  # El valor predeterminado es 100, pero puedes ajustarlo según lo veas necesario.
+    users = await mongo['usuarios'].find().to_list(limit)
+    if users is None:
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND, detail="No users found.")
+    for user in users:
+        user.pop('_id', None)  # aca uso None como valor predeterminado para evitar errores si _id no existe
+    return users
+# Update
+@app.put('/usuario/{user_id}')
+async def update_user(user_id: str, username: str, email: str, password: str,role: str):
+    updated_user = await mongo['usuarios'].find_one_and_replace({'user_id': user_id}, {'username': username,
+    'email': email, 
+    'password': password,
+    'role': role})
+    if updated_user is None:
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND)
+    updated_user.pop('_id')
+    return updated_user
+# Delete
+@app.delete('/usuario/{user_id}')
+async def delete_user(user_id: str):
+    deleted_user = await mongo['usuarios'].delete_one({'user_id': user_id})
+    if deleted_user.deleted_count == 0:
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND)
+    return {"status": "success"}
--- app/models.py
+++ app/models.py
@@ -3,3 +3,15 @@ from pydantic import BaseModel
 class Order(BaseModel):
     restaurant_id: str
     dishes: list[dict]
+
+class Pedido(BaseModel):
+    pedido_id: str
+    estado: bool
+
+    
+class User(BaseModel):
+    user_id: str
+    username: str
+    email: str # cambiar a tipo email con alguna libreria como pydantic
+    password: str
+    role: str  # roles posibles: ["cliente", "administrador", "encargado_despacho", "dueÃ±o"]
